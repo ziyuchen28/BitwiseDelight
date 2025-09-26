fb67d6cd91a69b0ae2f98436c2a00328376a4eaa
ziyuchen28 <zychenllp@gmail.com>
2025-09-25 20:47:27 -0700

    base bitwise

---
+++ src/bit.cpp
@@ -0,0 +1,43 @@
+#include "bit.h"
+
+
+bool is_even(int n) {
+    return (n & 1) == 0;
+}
+
+bool is_bit_set(int n, int k) {
+    return (n & (1 << k)) != 0;
+}
+
+void set_bit(int &n, int k) {
+    n |= (1 << k);
+}
+
+void clear_bit(int &n, int k) {
+    n &= ~(1 << k);
+}
+
+void xor_swap(int &a, int &b) {
+    if (&a == &b) return;
+    a ^= b;
+    b ^= a;
+    a ^= b;
+}
+
+int count_set_bits(int n) {
+    int count = 0;
+    while (n > 0) {
+        n &= (n - 1);
+        count++;
+    }
+    return count;
+}
+
+
+bool is_power_of_two(int n) {
+    return n > 0 && (n & (n - 1)) == 0;
+}
+
+int get_lowest_set_bit(int n) {
+    return n & -n;
+}
---
+++ src/bit.h
@@ -0,0 +1,14 @@
+#ifndef BIT_MANIP_LIB_H
+#define BIT_MANIP_LIB_H
+
+
+bool is_even(int n);
+bool is_bit_set(int n, int k);
+void set_bit(int &n, int k);
+void clear_bit(int &n, int k);
+void xor_swap(int &a, int &b);
+int count_set_bits(int n);
+bool is_power_of_two(int n);
+int get_lowest_set_bit(int n);
+
+#endif // BIT_MANIP_LIB_H
